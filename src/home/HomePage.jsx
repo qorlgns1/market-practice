5ab561107119828dbef15efaaddf9ae63956cdfd
qorlgns1 <rlgns0610@gmail.com>
2022-06-24 09:16:10 +0900

    fix: global.css가 전역적으로 적용되지 않아서 수정
    기존에 HomePage.jsx에 있는 global.css를 index.js로 옮기면서 global.css가 적용되게 했습니다.

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,28 +1,13 @@
 import React, { useState } from "react";
-import { createGlobalStyle } from "styled-components";
 import LodingPage from "./LodingPage";
-import reset from "styled-reset";
 import InitialLoginPage from "./InitialLoginPage";
-
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-
-  a {
-    text-decoration: none;
-  }
-`;
 
 const HomePage = () => {
   const [isLoding, setIsLoding] = useState(false);
   setTimeout(() => {
     setIsLoding(true);
   }, 300);
-  return (
-    <>
-      <GlobalStyle />
-      {!isLoding ? <LodingPage /> : <InitialLoginPage />}
-    </>
-  );
+  return <>{!isLoding ? <LodingPage /> : <InitialLoginPage />}</>;
 };
 
 export default HomePage;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style/global.css";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  a {
+    text-decoration: none;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
